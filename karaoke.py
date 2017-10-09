36a3a9af90c843282d3649a959f0ffab1ce0682e
Ana Cuevas Bravo <acuevas@gamma06.aulas.gsyc.urjc.es>
2017-10-09 17:40:20 +0200

    inicio de clase

--- karaoke.py
+++ karaoke.py
@@ -8,21 +8,39 @@ import sys
 import smallsmilhandler
 import json
 
+class KaraokeLocal(SmallSMILHandler):
+
+    def __init__(self, fichero):
+        parser = make_parser()
+        smilhandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(smilhandler)
+        parser.parse(open(fichero))
+        self.etiquetas = smilhandler.get_tags()
+
+    def __str__(self):
+        for etiqueta in self.etiquetas:
+            lista = []
+            lista.append(etiqueta['name'])
+            for atributo in etiqueta:
+                if atributo != 'name' and etiqueta[atributo] != "":
+                    lista.append('\t' + atributo + '="' + etiqueta[atributo] +'"')
+        return(lista)
+
+    def to_json(self):
+        json.dump(selfetiquetas, open('karaoke.json', 'w'))
+
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
         sys.exit("Usage: python3 karaoke.py file.smil")
 
-    parser = make_parser()
-    smilhandler = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(smilhandler)
-    parser.parse(open(sys.argv[1]))
+    karaoke = KaraokeLocal(sys.argv[1])
 
     for etiqueta in smilhandler.etiquetas:
         lista = []
         lista.append(etiqueta['name'])
         for atributo in etiqueta:
-            if atributo != 'name':
+            if atributo != 'name' and etiqueta[atributo] != "":
                 lista.append('\t' + atributo + '="' + etiqueta[atributo] +'"')
                 if atributo == 'src' and not etiqueta[atributo].find('http'):
                     nombre = etiqueta[atributo]
