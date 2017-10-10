2fe485ce670f4d4c316ff454ccf2a5f16db76682
ana <anuskycuevas@gmail.com>
2017-10-10 18:05:18 +0200

    faltan argumentos del json, clase funciona

--- karaoke.py
+++ karaoke.py
@@ -8,7 +8,7 @@ import sys
 import smallsmilhandler
 import json
 
-class KaraokeLocal(SmallSMILHandler):
+class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
 
     def __init__(self, fichero):
         parser = make_parser()
@@ -18,16 +18,27 @@ class KaraokeLocal(SmallSMILHandler):
         self.etiquetas = smilhandler.get_tags()
 
     def __str__(self):
+        lista = []
         for etiqueta in self.etiquetas:
-            lista = []
+            #lista = []
             lista.append(etiqueta['name'])
             for atributo in etiqueta:
                 if atributo != 'name' and etiqueta[atributo] != "":
                     lista.append('\t' + atributo + '="' + etiqueta[atributo] +'"')
-        return(lista)
+            lista.append('\n')       
+        return(' '.join(lista))
 
     def to_json(self):
-        json.dump(selfetiquetas, open('karaoke.json', 'w'))
+        json.dump(self.etiquetas, open('karaoke.json', 'w'))
+    
+    def do_local(self):
+        print('entra en do_local')
+        for etiqueta in self.etiquetas:
+            for atributo in etiqueta:
+                if atributo == 'src' and not etiqueta[atributo].find('http'):
+                    print('entra en src')
+                    nombre = etiqueta[atributo]
+                    urlretrieve(nombre, nombre.split('/')[-1])
 
 if __name__ == "__main__":
 
@@ -35,15 +46,19 @@ if __name__ == "__main__":
         sys.exit("Usage: python3 karaoke.py file.smil")
 
     karaoke = KaraokeLocal(sys.argv[1])
+    print(karaoke.__str__())
+    karaoke.to_json()
+    karaoke.do_local()
 
-    for etiqueta in smilhandler.etiquetas:
-        lista = []
-        lista.append(etiqueta['name'])
-        for atributo in etiqueta:
-            if atributo != 'name' and etiqueta[atributo] != "":
-                lista.append('\t' + atributo + '="' + etiqueta[atributo] +'"')
-                if atributo == 'src' and not etiqueta[atributo].find('http'):
-                    nombre = etiqueta[atributo]
-                    urlretrieve(nombre, nombre.split('/')[-1])
-        print(' '.join(lista))
-    json.dump(smilhandler.etiquetas, open('karaoke.json', 'w'))
+#    for etiqueta in smilhandler.etiquetas:
+#        lista = []
+#        lista.append(etiqueta['name'])
+#        for atributo in etiqueta:
+#            if atributo != 'name' and etiqueta[atributo] != "":
+#                lista.append('\t' + atributo + '="' + etiqueta[atributo] +'"')
+#                if atributo == 'src' and not etiqueta[atributo].find('http'):
+#                    nombre = etiqueta[atributo]
+#                    urlretrieve(nombre, nombre.split('/')[-1])
+#        print(' '.join(lista))
+#    json.dump(smilhandler.etiquetas, open('karaoke.json', 'w'))
+
